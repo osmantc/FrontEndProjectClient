4200931031c7e40052c063bf6aa4b3fae657f9a8
osmantc <blackcalvus06@hotmail.com>
2022-07-15 01:15:29 +0300

    eight.2

--- src/app/app-pivot/app-pivot.component.ts
+++ src/app/app-pivot/app-pivot.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  NgModule, Component, enableProdMode, ChangeDetectionStrategy,
+} from '@angular/core';
+
+import { DxPivotGridModule } from 'devextreme-angular';
+import * as AspNetData from 'devextreme-aspnet-data-nojquery';
 
 @Component({
   selector: 'app-pivot',
   templateUrl: './app-pivot.component.html',
   styleUrls: ['./app-pivot.component.css']
 })
-export class AppPivotComponent implements OnInit {
+export class AppPivotComponent {
 
-  constructor() { }
+  dataSource: any;
+  url: string;
 
-  ngOnInit(): void {
+  constructor() {
+
+    this.url = "https://localhost:7127/api/MobilAku";
+
+    this.dataSource = {
+      remoteOperations: true,
+      store: AspNetData.createStore({
+        key: 'OrderID',
+        loadUrl: `${this.url}/MobilAkus`,
+      }),
+      fields: [{
+        caption: 'Category',
+        dataField: 'ProductCategoryName',
+        width: 250,
+        expanded: true,
+        sortBySummaryField: 'SalesAmount',
+        sortBySummaryPath: [],
+        sortOrder: 'desc',
+        area: 'row',
+      }, {
+        caption: 'Subcategory',
+        dataField: 'ProductSubcategoryName',
+        width: 250,
+        sortBySummaryField: 'SalesAmount',
+        sortBySummaryPath: [],
+        sortOrder: 'desc',
+        area: 'row',
+      }, {
+        caption: 'Product',
+        dataField: 'ProductName',
+        area: 'row',
+        sortBySummaryField: 'SalesAmount',
+        sortBySummaryPath: [],
+        sortOrder: 'desc',
+        width: 250,
+      }, {
+        caption: 'Date',
+        dataField: 'DateKey',
+        dataType: 'date',
+        area: 'column',
+      }, {
+        caption: 'Amount',
+        dataField: 'SalesAmount',
+        summaryType: 'sum',
+        format: 'currency',
+        area: 'data',
+      }, {
+        caption: 'Store',
+        dataField: 'StoreName',
+      }, {
+        caption: 'Quantity',
+        dataField: 'SalesQuantity',
+        summaryType: 'sum',
+      }, {
+        caption: 'Unit Price',
+        dataField: 'UnitPrice',
+        format: 'currency',
+        summaryType: 'sum',
+      }, {
+        dataField: 'Id',
+        visible: false,
+      }],
+    };
   }
-
 }
